f238bd0357499619c4f698578ae080b3f65fbe7a
Syed Shayan <shayansyed712@gmail.com>
2023-10-05 22:54:52 +0500

    using bodyparser and use css in express etc

--- day2/app.js
+++ day2/app.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express();
+const bodyParser = require('body-parser')
 const form = require('./auth/form')
+const path = require('path')
 
+
+app.use(express.static(path.join(process.cwd(), 'public'))) //define static content to allow to render in server
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use((req, res, next) => {    // Call next method
     req.data = 'Syed Shayan'
     next();
@@ -10,7 +15,5 @@ app.use((req, res, next) => {    // Call next method
 //     // res.send(req.url);
 //     res.send(req.data);
 // })
-
-
 app.use("/form", form)
 app.listen(3000);
--- day2/auth/form.js
+++ day2/auth/form.js
@@ -1,9 +1,14 @@
 const express = require('express');
-
+const path = require('path');
 const Router = express.Router();
 
-
 Router.get("/", (req, res) => {
-    res.send("<form><input type='text' name='data'></input><button>Submit</button></form>")
+    // res.send("<form action='/form/submit' method='POST'><input type='text' name='data'></input><button>Submit</button></form>")
+    res.sendFile(path.join(process.cwd(), 'views', 'form.html'))
+})
+
+Router.post("/submit", (req, res) => {
+    // res.send('Submitted');
+    res.send(req.body);   // use req.body  after body-parser
 })
 module.exports = Router;
